54b50492c4c77d4ab8d55406d55d66102c3165a8
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-05-17 07:40:49 +0530

    test

---
+++ tests/11_MouseOperation/01_Notes.md
@@ -0,0 +1,6 @@
+
+
+1. Mouse move
+2. double click 
+3. Right click 
+4. Drag and drop
---
+++ tests/11_MouseOperation/02_Mousemove.spec.ts
@@ -0,0 +1,19 @@
+import test from "@playwright/test";
+
+
+
+test("Mouse move operation",async({page})=>{
+
+
+
+      await page.goto("https://playwrightautomationtesting.blogspot.com/2025/01/menu-items.html")
+   
+      await page.getByText("Courses").hover()
+
+      await page.waitForTimeout(5000)
+
+      await page.getByText("JavaScript",{exact:true}).click()
+
+      await page.waitForTimeout(50000)
+
+})
