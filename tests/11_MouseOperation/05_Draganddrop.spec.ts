e300a4bc0a05b926bc3f0a5f8da1ea0dec6e44ce
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-05-20 07:10:38 +0530

    test

--- tests/11_MouseOperation/05_Draganddrop.spec.ts
+++ tests/11_MouseOperation/05_Draganddrop.spec.ts
@@ -4,12 +4,13 @@ test("Drag and drop operation",async({page})=>{
 
    await page.goto("https://playwrightautomationtesting.blogspot.com/2025/01/drag-and-drop-operation.html")
   
-   const sourceelement=page.locator("#dragImage")
+   // const sourceelement=page.locator("#dragImage")
 
-   const targetelement=page.locator("#target>img")
+   // const targetelement=page.locator("#target>img")
 
-   await sourceelement.dragTo(targetelement) // drag and drop operat
+  //await sourceelement.dragTo(targetelement) // drag and drop operat
 
+   await page.dragAndDrop("#dragImage","#target>img")
 
    await page.waitForTimeout(1000)
 
