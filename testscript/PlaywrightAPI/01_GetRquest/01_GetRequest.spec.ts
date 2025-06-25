da76afa67c4fb144b8d53fd3471706a5e13ca6ca
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-06-25 08:31:41 +0530

    test

--- testscript/PlaywrightAPI/01_GetRquest/01_GetRequest.spec.ts
+++ testscript/PlaywrightAPI/01_GetRquest/01_GetRequest.spec.ts
@@ -3,16 +3,24 @@ import test, { expect } from "@playwright/test";
 
 test("get request",async({request})=>{
 
+
+const token="ghp_zi8bW"
+
 const response=await request.get("https://api.github.com/orgs/SenthilPlaywrightAutomation/repos",
 {
  headers:
  {
-    'Accept':'application/vnd.github+json'
+    'Accept':'application/vnd.github+json',
+    'Authorization':`Bearer ${token}`
  }
 }
 )
 expect(response.status()).toBe(200)
 expect(response.ok()).toBeTruthy()
-console.log(await response.json())
+const repos=await response.json()
+
+repos.forEach(res => {
+   console.log(res.name,res.private)
+});
 
 })
